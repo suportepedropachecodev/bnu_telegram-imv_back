c7b9053e62b89f3d92e25145ca61b0ff1446d48a
Pedro A Pacheco <suporte@pedropachecodev.com.br>
2021-07-29 17:39:47 -0300

    Producao sem headless

--- src/wspg.js
+++ src/wspg.js
@@ -11,7 +11,7 @@ const wspg = async () => {
 
     const browser = await puppeteer.launch({
         args: ['--no-sandbox'],
-        headless: false,
+        //headless: false,
     });
     const page = await browser.newPage();
 
