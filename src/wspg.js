3b96a01c812b1692c5685114c47102d584db46e1
Pedro A Pacheco <suporte@pedropachecodev.com.br>
2021-07-29 11:46:39 -0300

    Inicio - WS de imovel unico pronto

---
+++ src/wspg.js
@@ -0,0 +1,36 @@
+//Extrai dados individuais de pagina
+
+const puppeteer = require('puppeteer');
+
+const urlalvo = 'https://sc.olx.com.br/norte-de-santa-catarina/imoveis/blumenau-apartamento-padrao-itoupava-seca-896470360';
+
+let detalhesImovel = [];
+
+const wspg = async () => {
+
+    const browser = await puppeteer.launch({
+        headless: false,
+    });
+    const page = await browser.newPage();
+
+    await page.goto(urlalvo);
+    await page.waitForTimeout(3000);
+    const nome = await page.$eval('.sc-45jt43-0.eCghYu.sc-ifAKCX.cmFKIN', (el) => el.textContent);
+    const valor = await page.$eval('.sc-1wimjbb-0.JzEH.sc-ifAKCX.cmFKIN', (el) => el.textContent);
+    const codigobruto = await page.$eval('.sc-16iz3i7-0.qJvUT.sc-ifAKCX.fizSrB', (el) => el.textContent);
+    const codigo = codigobruto.replace(/\D/gim, '');//Limpa string de codigo pegando apenas numeros 
+    const urlimovel = urlalvo;
+    const detalhesArray = await page.$$('.duvuxf-0.h3us20-0.jyICCp');
+    for (let detalhesElement of detalhesArray) {
+        let detalhesImv = await detalhesElement.$eval('.sc-hmzhuo.sc-1f2ug0x-3.ONRJp.sc-jTzLTM.iwtnNi', element => element.innerText);
+        const detalhes = await detalhesImv.replace('\n', ' : ');
+        detalhesImovel.push(detalhes)
+    }
+
+    await console.log({ nome, valor, urlimovel, codigo, detalhesImovel });
+
+
+
+    await browser.close();
+};
+wspg();
